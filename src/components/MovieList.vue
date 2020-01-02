b9950d7403383c62e7ff1d930acdd5b92ffb99ab
Dmitriy <d.lakhno@gmail.com>
2020-01-02 19:42:19 +0600

    Add bus, route and slot

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,35 +1,79 @@
 <template>
     <div id="movie-list">
-        <div v-for="movie in filteredMovies" class="movie">{{ movie.title }}</div>
+        <div v-if="filteredMovies.length">
+            <movie-item v-for="movie in filteredMovies" v-bind:movie="movie.movie">
+                <div class="movie-sessions">
+                    <div v-for="session in filteredSessions(movie.sessions)" class="session-time-wrapper">
+                        <div class="session-time">{{ formatSessionTime(session.time) }}</div>
+                    </div>
+                </div>
+            </movie-item>
+        </div>
+        <div v-else-if="movies.length" class="no-results">
+            {{ noResults }}
+        </div>
+        <div v-else class="no-results">
+            Loading...
+        </div>
     </div>
 </template>
 <script>
     import genres from '../util/genres';
+    import times from '../util/times';
+    import MovieItem from './MovieItem.vue';
 
     export default {
-        data () {
-            return {
-                movies: [
-                    {title: 'Pulp Fiction', genre: genres.CRIME},
-                    {title: 'Home Alone', genre: genres.COMEDY},
-                    {title: 'Austin Powers', genre: genres.COMEDY}
-                ]
-            };
-        },
-        props: ['genre', 'time'],
+        props: ['genre', 'time', 'movies', 'day'],
         methods: {
             moviePassesGenreFilter(movie) {
                 if(!this.genre.length){
                     return true;
                 } else {
-                    return this.genre.find(genre => movie.genre === genre);
+                    let movieGenres = movie.movie.Genre.split(", ");
+                    let matched = true;
+                    this.genre.forEach(genre => {
+                        if(movieGenres.indexOf(genre) === -1) {
+                            matched = false;
+                        }
+                    });
+                    return matched;
+                }
+            },
+            formatSessionTime(raw) {
+                return this.$moment(raw).format('h:mm A');
+            },
+            filteredSessions(sessions) {
+                return sessions.filter(this.sessionPassesTimeFilter);
+            },
+            sessionPassesTimeFilter(session) {
+                if (!this.day.isSame(this.$moment(session.time), 'day')) {
+                    return false;
+                } else if (this.time.length === 0 || this.time.length === 2){
+                    return true;
+                } else if (this.time[0] === times.AFTER_6PM) {
+                    return this.$moment(session.time).hour() >= 18;
+                } else {
+                    return this.$moment(session.time).hour() < 18;
                 }
             }
         },
         computed: {
             filteredMovies () {
-                return this.movies.filter(this.moviePassesGenreFilter);
+                return this.movies
+                    .filter(this.moviePassesGenreFilter)
+                    .filter(movie => movie.sessions.find(this.sessionPassesTimeFilter));
+            },
+            noResults () {
+                let times = this.time.join(', ');
+                let genres = this.genre.join(', ')
+                return `No results for ${times}${ times.length && genres.length ? ', ' : ' '} ${genres}.`
             }
+        },
+        components: {
+            MovieItem
+        },
+        created() {
+            // console.log(this.$moment);
         }
     }
 </script>
